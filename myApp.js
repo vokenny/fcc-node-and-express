3258785cae602e4f9d9096ddedb52999c327262b
Kenny Vo <kennyvo208@gmail.com>
2023-07-04 14:58:14 +0100

    fix: respond with json

--- myApp.js
+++ myApp.js
@@ -3,14 +3,20 @@ let app = express();
 
 console.log('Hello World');
 
+const usePublicAssets = () => express.static(__dirname + '/public');
+
 function serveHomePage(_, res) {
   const absolutePathHome = __dirname + '/views/index.html';
   res.sendFile(absolutePathHome);
 }
 
-const usePublicAssets = () => express.static(__dirname + '/public');
+function serveJsonHello(_, res) {
+  const helloMessage = { message: 'Hello json' };
+  res.json(helloMessage);
+}
 
 app.use('/public', usePublicAssets());
 app.get('/', serveHomePage);
+app.get('/json', serveJsonHello);
 
 module.exports = app;
